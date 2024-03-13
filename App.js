a86ae1711164fbc0865f4ca7eef5ceb2c0412279
pogromcakaszy <pogromcakaszy@gmail.com>
2024-03-13 12:55:43 +0100

    Update

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { useFonts } from 'expo-font';
 import { useCallback } from 'react';
-
+import * as Updates from 'expo-updates';
 
 export default function App() {
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
 
   const [isLoaded] = useFonts({
     'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
